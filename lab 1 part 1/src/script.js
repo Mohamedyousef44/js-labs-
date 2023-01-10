5afa665f40060ab75b80cdb90e31c31ec48dc4f2
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-01-10 14:06:25 +0200

    js labs

---
+++ lab 1 part 1/src/script.js
@@ -0,0 +1,11 @@
+
+let fname = prompt("please enter ur first name","mohamed");
+let lname = prompt("please enter ur last name","yossef");
+let  nameStatus  = confirm(`your full name is ${fname}+${lname}`);
+let bod = prompt("please enter ur birth of date ",26);
+document.write(`welcome ${fname +" "+ lname} your are ${bod} years old` )
+
+
+
+
+
---
+++ lab 1 part 2/src/script.js
@@ -0,0 +1,5 @@
+alert("that is the first realese of a calculator that has only sum 2 numbers");
+let num1 = prompt("please insert ur 1st number ",0);
+let num2 = prompt("please insert ur 2nd number ",0);
+let sum = Number(num1) + Number(num2);
+document.write(`${num1} + ${num2} = ${sum} `)

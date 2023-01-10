d63d444a56c8841b28ac37f31fa413d812b970d2
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-01-10 18:45:38 +0200

    modified it to be more reialble

--- lab 1 part 1/src/script.js
+++ lab 1 part 1/src/script.js
@@ -1,8 +1,18 @@
 
-let fname = prompt("please enter ur first name","mohamed");
-let lname = prompt("please enter ur last name","yossef");
-let  nameStatus  = confirm(`your full name is ${fname}+${lname}`);
-let bod = prompt("please enter ur birth of date ",26);
+var nameStatus = false ;
+var fname = null ;
+var lname = null ;
+var bod = null;
+
+while(!nameStatus){
+ fname = prompt("please enter ur first name","mohamed");
+ lname = prompt("please enter ur last name","yossef");
+ nameStatus  = confirm(`your full name is ${fname}+${lname}`);
+}
+
+while(bod == null){
+bod = prompt("please enter ur birth of date ",26);
+}
 document.write(`welcome ${fname +" "+ lname} your are ${bod} years old` )
 
 
